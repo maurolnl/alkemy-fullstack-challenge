c5297937c4fce50fe355723c4884f1c9bbb6a5b9
maurolnl <mauroleoquiroga@gmail.com>
2022-06-28 15:36:12 -0300

    update path to static files

--- api/src/app.js
+++ api/src/app.js
@@ -1,3 +1,5 @@
+import path from 'path';
+
 import express from 'express';
 import { config } from 'dotenv';
 import cors from 'cors';
@@ -5,6 +7,7 @@ import cors from 'cors';
 import operationRoutes from './routes/operation.routes';
 
 const app = express();
+const react_path = path.join(__dirname, '..', '..', 'app', 'dist');
 
 //settings
 app.set('port', process.env.PORT || 3001);
@@ -12,7 +15,7 @@ app.set('port', process.env.PORT || 3001);
 //middlewares
 app.use(express.json());
 app.use(cors());
-app.use(express.static('../../app/dist/index.html'));
+app.use(express.static(react_path));
 
 config();
 

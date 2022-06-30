502dca19a56b20ab2cca8a1358a4b50c3a7e98e0
maurolnl <mauroleoquiroga@gmail.com>
2022-06-30 14:24:14 -0300

    update navbar buttons

--- app/src/common/Navbar/index.tsx
+++ app/src/common/Navbar/index.tsx
@@ -1,19 +1,42 @@
-import {Stack, Text, Box} from "@chakra-ui/react";
+import {Stack, Box} from "@chakra-ui/react";
+import {AiOutlineHome, AiOutlineUserDelete, AiOutlinePlusCircle} from "react-icons/ai";
+import {HiOutlineInbox} from "react-icons/hi";
 
+import GradientButton from "../Button";
 import Layout from "../Layout";
 
 const Navbar = () => {
   return (
     <Box as="header" backgroundColor="neutral.white">
       <Layout>
-        <Stack alignItems="center" direction="row" justifyContent="space-between" minH="137px">
+        <Stack alignItems="center" direction="row" justifyContent="space-between" minH="100px">
           <Stack as="nav" direction="row">
-            <Text>Home</Text>
-            <Text>List</Text>
+            <GradientButton
+              ariaLabel="Home"
+              hasLabel={true}
+              icon={<AiOutlineHome />}
+              url="/"
+              variant="active"
+            />
+            <GradientButton
+              ariaLabel="Ver Operaciones"
+              hasLabel={true}
+              icon={<HiOutlineInbox />}
+              url="/list-operations"
+            />
           </Stack>
           <Stack direction="row">
-            <Text>Crear</Text>
-            <Text>Avatar</Text>
+            <GradientButton
+              ariaLabel="Crear Operación"
+              hasLabel={true}
+              icon={<AiOutlinePlusCircle />}
+            />
+            <GradientButton
+              ariaLabel="Cerrar Sesión"
+              hasLabel={true}
+              icon={<AiOutlineUserDelete />}
+              url="/login"
+            />
           </Stack>
         </Stack>
       </Layout>
